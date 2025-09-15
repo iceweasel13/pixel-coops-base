88455284952bbde242d6365e06107243ab708c48
Sinanzaman <sinanhesap55@gmail.com>
2025-09-15 17:00:48 +0300

    butonlar büyüdü, giriş ekranı responsive oldu

--- src/components/BalanceDisplay.tsx
+++ src/components/BalanceDisplay.tsx
@@ -10,7 +10,7 @@ export type Balance = {
 export function BalanceDisplay({ balance }: { balance: Balance }) {
   return (
     <div className="bg-gray-800 bg-opacity-75 text-white p-2 rounded-md flex items-center gap-2 w-32">
-      <Image src={balance.icon} alt={balance.name} width={24} height={24} />
+      <Image src={balance.icon} alt={balance.name} width={30} height={30} />
       <span className="font-bold text-lg flex-grow text-center">{balance.amount}</span>
     </div>
   );
