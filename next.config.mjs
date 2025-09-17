3541d2d2587f82b4e1284d37d33927df4749ef0e
Nihat Zaman <nihat.zaman78@erzurum.edu.tr>
2025-09-17 10:13:33 +0300

    fix v11

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: { unoptimized: true }
+    images: { unoptimized: true },
+      transpilePackages: ['@rainbow-me/rainbowkit', 'wagmi'],
 };
 
 export default nextConfig;
