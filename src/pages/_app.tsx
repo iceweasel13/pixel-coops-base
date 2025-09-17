e9d85fde3d1d5d6ce3aee09cf4789b22f3852bd3
Nihat Zaman <nihat.zaman78@erzurum.edu.tr>
2025-09-17 09:08:43 +0300

    deploymentfixv7

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,48 +1,20 @@
 import "@/styles/globals.css";
-import type { AppContext, AppProps } from "next/app";
-import App from "next/app";
+import type { AppProps } from "next/app";
 import Providers from "../components/auth/Provider";
-import { getSession } from "next-auth/react";
-import type { Session } from "next-auth";
-
 import { GameProvider } from "@/context/GameContext";
 
-type MyAppPageProps = AppProps["pageProps"] & {
-  session?: Session | null;
-  cookie?: string;
-};
 
-function MyApp({
-  Component,
-  pageProps,
-}: AppProps<MyAppPageProps>) {
-  const { session, cookie, ...restPageProps } = pageProps;
-
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Providers session={session ?? undefined} cookie={cookie}>
+   
+    <Providers session={pageProps.session}>
       <GameProvider>
-        <Component {...restPageProps} />
+        <Component {...pageProps} />
       </GameProvider>
     </Providers>
   );
 }
 
-MyApp.getInitialProps = async (appContext: AppContext) => {
-  const appProps = await App.getInitialProps(appContext);
-  const { ctx } = appContext;
-
-  const session = await getSession(ctx);
-  const cookie = ctx.req?.headers.cookie ?? "";
-
-  return {
-    ...appProps,
-    pageProps: {
-      ...appProps.pageProps,
-      session,
-      cookie,
-    },
-  };
-};
+// MyApp.getInitialProps bloğunu TAMAMEN SİLİYORUZ.
 
 export default MyApp;
-
